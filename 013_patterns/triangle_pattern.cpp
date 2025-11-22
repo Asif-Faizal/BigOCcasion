1ff5a2a4e80463ff1f1b1eaa1c95215d3a107a62
asif <asif@loanwiser.in>
2025-11-22 13:51:27 +0530

    feat: Implement various triangle pattern printing examples including right angle, number, character

---
+++ 013_patterns/triangle_pattern.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+using namespace std;
+
+// right angle triangle
+// *
+// **
+// ***
+// ****
+// int main (){
+//     int n;
+//     cout << "Enter size of triangle: ";
+//     cin >> n;
+//     for (int i = 0;i< n; i++){
+//         for (int j = 0; j <= i; j++){
+//             cout << "* ";
+//         }
+//         cout << endl;
+//     }
+// }
+
+// int main (){
+//     int n;
+//     cout << "Enter size of triangle: ";
+//     cin >> n;
+//     for (int i = 0;i< n; i++){
+//     char ch = 'A';
+//         for (int j = 0; j <= i; j++){
+//             cout << ch << " ";
+//             ch++;
+//         }
+//         cout << endl;
+//     }
+// }
+
+// int main (){
+//     int n;
+//     cout << "Enter size of triangle: ";
+//     cin >> n;
+//     for (int i = 0;i< n; i++){
+//     int num = 1;
+//         for (int j = 0; j <= i; j++){
+//             cout << num << " ";
+//             num++;
+//         }
+//         cout << endl;
+//     }
+// }
+
+// int main (){
+//     int n;
+//     cout << "Enter size of triangle: "; // backwards => i+1 to the 0
+//     cin >> n;
+//     for (int i = 0;i< n; i++){
+//         for (int j = i+1; j >0; j--){
+//             cout << j << " ";
+//         }
+//         cout << endl;
+//     }
+// }
+
+// FLOYD'S TRIANGLE
+// int main (){
+//     int n;
+//     cout << "Enter size of triangle: ";
+//     cin >> n;
+//     int num = 1;
+//     for (int i = 0;i< n; i++){
+//         for (int j = i+1; j >0; j--){
+//             cout << num << " ";
+//             num++;
+//         }
+//         cout << endl;
+//     }
+// }
+// int main (){
+//     int n;
+//     cout << "Enter size of triangle: ";
+//     cin >> n;
+//     char ch = 'A';
+//     for (int i = 0;i< n; i++){
+//         for (int j = i+1; j >0; j--){
+//             cout << ch << " ";
+//             ch++;
+//         }
+//         cout << endl;
+//     }
+// }
